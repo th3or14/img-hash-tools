1ea343049c9ffa6831c52e33edcc5527392567d3
th3or14 <th3or14@gmail.com>
2020-07-28 06:41:47 +0300

    do not build image each time

--- docker/start.sh
+++ docker/start.sh
@@ -5,7 +5,9 @@ CONTAINER_NAME=img-hash-tools
 MOUNT_SRC=`pwd`
 MOUNT_TARGET=/shared-folder
 
-docker build -t $IMAGE_NAME .
+if [ "$(docker image inspect $IMAGE_NAME 2> /dev/null)" == [] ]; then
+    docker build -t $IMAGE_NAME .
+fi
 docker rm -f $CONTAINER_NAME > /dev/null 2>&1
 xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $CONTAINER_NAME 2> /dev/null`
 docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --mount src=$MOUNT_SRC,target=$MOUNT_TARGET,type=bind --name $CONTAINER_NAME $IMAGE_NAME
