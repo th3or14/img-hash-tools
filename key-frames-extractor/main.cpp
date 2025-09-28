9aac543b6dbcd4893f73001990590cfcf29a581a
th3or14 <37025359+th3or14@users.noreply.github.com>
2025-09-28 23:38:45 +0300

    Remove semicolon after newline

--- key-frames-extractor/main.cpp
+++ key-frames-extractor/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     {
         std::cout << "Wrong number of arguments (given " + std::to_string(argc - 1) +
                      ", expected " + std::to_string(expected_argc - 1) +
-                     ").\nRequired arguments:\n\tinput video filename\n\toutput location\n;";
+                     ").\nRequired arguments:\n\tinput video filename\n\toutput location\n";
         return EXIT_FAILURE;
     }
     extract_key_frames(argv[1], argv[2]);
