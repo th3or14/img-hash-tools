2fe86d06c722fc4bbd06a80ce292c9e6cfe5f986
th3or14 <37025359+th3or14@users.noreply.github.com>
2026-08-23 03:08:13 +0300

    Build only img_hash module

--- install-opencv.sh
+++ install-opencv.sh
@@ -26,12 +26,14 @@ cd ..
 cd opencv_contrib
 git reset --hard 5.0.0
 cd ..
+mkdir -p only_img_hash_module
+cp -rf opencv_contrib/modules/img_hash only_img_hash_module
 RepoSource=opencv
 mkdir -p build_opencv
 pushd build_opencv
 CMAKE_OPTIONS=(-DBUILD_PERF_TESTS:BOOL=OFF -DBUILD_TESTS:BOOL=OFF -DBUILD_DOCS:BOOL=OFF  -DWITH_CUDA:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DINSTALL_CREATE_DISTRIB=ON)
 set -x
-cmake "${CMAKE_GENERATOR_OPTIONS[@]}" "${CMAKE_OPTIONS[@]}" -DOPENCV_EXTRA_MODULES_PATH="$myRepo"/opencv_contrib/modules -DCMAKE_INSTALL_PREFIX="$myRepo/install/$RepoSource" "$myRepo/$RepoSource"
+cmake "${CMAKE_GENERATOR_OPTIONS[@]}" "${CMAKE_OPTIONS[@]}" -DOPENCV_EXTRA_MODULES_PATH="$myRepo"/only_img_hash_module -DCMAKE_INSTALL_PREFIX="$myRepo/install/$RepoSource" "$myRepo/$RepoSource"
 echo "************************* $Source_DIR -->debug"
 cmake --build .  --config debug
 echo "************************* $Source_DIR -->release"
