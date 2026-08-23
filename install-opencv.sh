46f30a9fd36c0407978f1380b2bd0efd809b03f1
th3or14 <37025359+th3or14@users.noreply.github.com>
2026-08-23 21:44:50 +0300

    Remove double spaces and omit :<type>

--- install-opencv.sh
+++ install-opencv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 myRepo=$(pwd)
 CMAKE_GENERATOR_OPTIONS=-G"Visual Studio 18 2026"
-if [  ! -d "$myRepo/opencv"  ]; then
+if [ ! -d "$myRepo/opencv" ]; then
     echo "cloning opencv"
     git clone https://github.com/opencv/opencv.git
 else
@@ -9,7 +9,7 @@ else
     git pull --rebase
     cd ..
 fi
-if [  ! -d "$myRepo/opencv_contrib"  ]; then
+if [ ! -d "$myRepo/opencv_contrib" ]; then
     echo "cloning opencv_contrib"
     git clone https://github.com/opencv/opencv_contrib.git
 else
@@ -28,10 +28,10 @@ cp -rf opencv_contrib/modules/img_hash only_img_hash_module
 RepoSource=opencv
 mkdir -p build_opencv
 pushd build_opencv
-CMAKE_OPTIONS=(-DBUILD_opencv_world:BOOL=OFF -DBUILD_LIST:STRING=img_hash,videoio,imgcodecs -DBUILD_PERF_TESTS:BOOL=OFF -DBUILD_TESTS:BOOL=OFF -DBUILD_DOCS:BOOL=OFF  -DWITH_CUDA:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DINSTALL_CREATE_DISTRIB=ON)
+CMAKE_OPTIONS=(-DBUILD_opencv_world=OFF -DBUILD_LIST=img_hash,videoio,imgcodecs -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_DOCS=OFF -DWITH_CUDA=OFF -DBUILD_EXAMPLES=OFF -DINSTALL_CREATE_DISTRIB=ON)
 set -x
 cmake "${CMAKE_GENERATOR_OPTIONS[@]}" "${CMAKE_OPTIONS[@]}" -DOPENCV_EXTRA_MODULES_PATH="$myRepo"/only_img_hash_module -DCMAKE_INSTALL_PREFIX="$myRepo/install/$RepoSource" "$myRepo/$RepoSource"
 echo "************************* $Source_DIR -->release"
-cmake --build .  --config release
-cmake --build .  --target install --config release
+cmake --build . --config release
+cmake --build . --target install --config release
 popd
