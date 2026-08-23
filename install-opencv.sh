46b4c6597aac72a4e9573ff0938e338e308a1646
th3or14 <37025359+th3or14@users.noreply.github.com>
2026-08-23 06:31:03 +0300

    Remove comments

--- install-opencv.sh
+++ install-opencv.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -e
 myRepo=$(pwd)
 CMAKE_GENERATOR_OPTIONS=-G"Visual Studio 18 2026"
-#CMAKE_GENERATOR_OPTIONS=-G"Visual Studio 16 2019"
-#CMAKE_GENERATOR_OPTIONS=-G"Visual Studio 15 2017 Win64"
-#CMAKE_GENERATOR_OPTIONS=(-G"Visual Studio 16 2019" -A x64)  # CMake 3.14+ is required
 if [  ! -d "$myRepo/opencv"  ]; then
     echo "cloning opencv"
     git clone https://github.com/opencv/opencv.git
@@ -34,10 +31,7 @@ pushd build_opencv
 CMAKE_OPTIONS=(-DBUILD_opencv_dnn:BOOL=OFF -DBUILD_PERF_TESTS:BOOL=OFF -DBUILD_TESTS:BOOL=OFF -DBUILD_DOCS:BOOL=OFF  -DWITH_CUDA:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DINSTALL_CREATE_DISTRIB=ON)
 set -x
 cmake "${CMAKE_GENERATOR_OPTIONS[@]}" "${CMAKE_OPTIONS[@]}" -DOPENCV_EXTRA_MODULES_PATH="$myRepo"/only_img_hash_module -DCMAKE_INSTALL_PREFIX="$myRepo/install/$RepoSource" "$myRepo/$RepoSource"
-#echo "************************* $Source_DIR -->debug"
-#cmake --build .  --config debug
 echo "************************* $Source_DIR -->release"
 cmake --build .  --config release
 cmake --build .  --target install --config release
-#cmake --build .  --target install --config debug
 popd
